1ea68037a657bab8d6e1e57187634b2f529fb73b
lmaiorano <158139675+Pmaioranool@users.noreply.github.com>
2025-11-27 12:13:11 +0100

    feat(vanilla js)save localstorage favorites

--- VANILLA/JavaScript/script.js
+++ VANILLA/JavaScript/script.js
@@ -29,7 +29,6 @@ async function fetchWeatherData(city) {
 function showData(data) {
   // afficher les données du temps actuel
   const currentWeather = document.getElementById("currentWeather");
-  console.log(data);
   let description = "";
   data.weather.forEach((element) => {
     description += element.description + ", ";
@@ -39,7 +38,8 @@ function showData(data) {
 
   currentWeather.children[0].innerText = description;
   currentWeather.children[1].innerHTML = `${Math.round(data.main.temp)} °C`;
-  currentWeather.children[2].innerText = `${data.name}, ${data.sys.country}`;
+  currentWeather.children[2].children[0].innerText = `${data.name}`;
+  currentWeather.children[2].children[1].innerText = `, ${data.sys.country}`;
 
   // afficher les données du temps précis
 
@@ -52,8 +52,37 @@ function showData(data) {
     data.visibility / 1000
   ).toFixed(1)} km`;
 }
+
+function save(list, item) {
+  const oldList = localStorage.getItem(item)
+    ? JSON.parse(localStorage.getItem(item))
+    : [];
+
+  const ONE_DAY_MS = 24 * 60 * 60 * 1000; // Millisecondes dans un jour
+
+  if (item === "history") {
+    // Vérifier si un élément avec le même nom existe à moins d'un jour
+    const existsRecent = oldList.find(
+      (el) =>
+        el.name === list.name &&
+        Math.abs(new Date(list.date) - new Date(el.date)) < ONE_DAY_MS
+    );
+    if (existsRecent) return;
+  } else {
+    // Vérifier juste par nom pour les autres listes
+    if (oldList.find((el) => el.name === list.name)) return;
+  }
+
+  // Limiter à 10 éléments
+  if (oldList.length >= 10) oldList.pop();
+
+  const newList = [list, ...oldList];
+  localStorage.setItem(item, JSON.stringify(newList));
+}
+
 import API_KEY from "../env.js";
 
+// Vérification de la clé API
 if (
   !API_KEY ||
   API_KEY === undefined ||
@@ -83,8 +112,24 @@ form.addEventListener("submit", async (event) => {
   const data = await fetchWeatherData(city);
   if (data) {
     showData(data);
+    save(data, "history");
   }
 });
+
+const favoriteBtn = document.getElementById("favoriteBtn");
+favoriteBtn.addEventListener("click", () => {
+  const cityName = document.getElementById("cityName").innerText;
+
+  if (!cityName || cityName === "Ville") {
+    toast("Aucune ville à ajouter aux favoris.", "warning");
+    return;
+  }
+  save(cityName, "favorites");
+  toast(`${cityName} ajoutée aux favoris !`, "success");
+  showFavorites();
+});
+
+// Test initial avec une ville par défaut
 
 fetchWeatherData("Paris").then((data) => {
   if (data) {
