48268b3dc20afbb72e731c6929215f333b618a46
lmaiorano <158139675+Pmaioranool@users.noreply.github.com>
2025-11-27 11:48:06 +0100

    feat(vanilla js) write data

--- VANILLA/JavaScript/script.js
+++ VANILLA/JavaScript/script.js
@@ -8,33 +8,50 @@ function toast(message, type = "info", duration = 3000) {
   }, duration);
 }
 
-function fetchWeatherData(city) {
+async function fetchWeatherData(city) {
   try {
-    fetch(
+    const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric&lang=fr`
-    )
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Ville non trouvée");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-        // Traitez et affichez les données météo ici
-      })
-      .catch((error) => {
-        toast(error.message, "error");
-      });
-  } catch (error) {
-    toast(
-      "Une erreur est survenue lors de la récupération des données.",
-      "error",
-      5000
     );
+
+    if (!response.ok) {
+      throw new Error("Ville non trouvée");
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    toast(error.message, "error");
+    return null; // important : permet de tester dans form.submit
   }
 }
 
+function showData(data) {
+  // afficher les données du temps actuel
+  const currentWeather = document.getElementById("currentWeather");
+  console.log(data);
+  let description = "";
+  data.weather.forEach((element) => {
+    description += element.description + ", ";
+  });
+  // Supprime la dernière virgule et l'espace
+  description = description.slice(0, -2);
+
+  currentWeather.children[0].innerText = description;
+  currentWeather.children[1].innerHTML = `${Math.round(data.main.temp)} °C`;
+  currentWeather.children[2].innerText = `${data.name}, ${data.sys.country}`;
+
+  // afficher les données du temps précis
+
+  const weatherGrid = document.getElementById("weatherGrid");
+  weatherGrid.children[0].children[1].innerText = `${data.main.feels_like} °C`;
+  weatherGrid.children[1].children[1].innerText = `${data.main.humidity} %`;
+  weatherGrid.children[2].children[1].innerText = `${data.main.pressure} hPa`;
+  weatherGrid.children[3].children[1].innerText = `${data.wind.speed} m/s`;
+  weatherGrid.children[4].children[1].innerText = `${(
+    data.visibility / 1000
+  ).toFixed(1)} km`;
+}
 import API_KEY from "../env.js";
 
 if (
@@ -55,7 +72,7 @@ if (
 
 const form = document.querySelector(".search-container");
 
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", async (event) => {
   event.preventDefault(); // empêche le rechargement
 
   const city = document.getElementById("searchInput").value;
@@ -63,5 +80,14 @@ form.addEventListener("submit", (event) => {
     toast("Veuillez entrer le nom d'une ville.", "warning");
     return;
   }
-  const data = fetchWeatherData(city);
+  const data = await fetchWeatherData(city);
+  if (data) {
+    showData(data);
+  }
+});
+
+fetchWeatherData("Paris").then((data) => {
+  if (data) {
+    showData(data);
+  }
 });
